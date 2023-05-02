79fcefdce845afd18dd093658938ba49f1325cf2
rpau <raquel@moderne.io>
2023-05-02 14:56:36 +0200

    Public domain license

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -5,7 +5,7 @@ class Mod < Formula
   homepage "https://moderne.io"
   url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v0.1.10/moderne-cli-macos-tar-v0.1.10"
   sha256 "c1ee09d2bed13530e5c55ac89f5dbc982ef99d1b0e1fdccc13017121098afd1b"
-  license "MIT"
+  license :public_domain
   version "v0.1.10"
 
   def install
