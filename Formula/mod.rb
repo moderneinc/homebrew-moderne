ccbf4ba07786ee8aba03f87c4f55d40bd1480bce
Tim te Beek <tim@moderne.io>
2023-12-04 15:27:25 +0100

    Point to staging build for brew

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -3,7 +3,7 @@
 class Mod < Formula
   desc "Automated code remediation."
   homepage "https://moderne.io"
-  url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.23/moderne-cli-macos-tar-v1.6.23"
+  url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.23/moderne-cli-macos-tar-v1.6.23"
   sha256 "4fac059ec57b113efa094e1687a13a39c9d2ecc6268d6e3150a04533be07fd3f"
   license :public_domain
   version "v1.6.23"
