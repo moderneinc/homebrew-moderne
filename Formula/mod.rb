3ed4c1de9672d73a3bd406825034fa6e4bfa0f35
Joan Viladrosa <joan@moderne.io>
2023-12-13 23:01:05 +0100

    Update mod.rb

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -5,11 +5,15 @@ class Mod < Formula
   homepage "https://moderne.io"
   url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.27/moderne-cli-macos-tar-v1.6.27"
   sha256 "d45a251e28030f348f19c413379a7755a85d49a9e465725f78d1185625f7b1d9"
-  license :public_domain
   version "v1.6.27"
+  license :public_domain
 
-  head "https://github.com/moderneinc/homebrew-moderne.git", branch: "staging" # the default is "main"
-
+  head do
+    url "https://github.com/moderneinc/homebrew-moderne.git", branch: "staging" # the default is "main"
+    sha256 "71465b344858440c9d9c9df8a6aed7979791930be12f00be0c08fc4c78725690"
+    version "v1.7.1"
+  end
+  
   def install
     bin.install "mod"
   end
