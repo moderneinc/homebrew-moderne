7b3a16edd9df23c92261938c136e1823464eb9ad
Joan Viladrosa <joan@moderne.io>
2023-12-13 22:24:39 +0100

    Revert stable to 1.6.27 and add head for staging

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -3,10 +3,12 @@
 class Mod < Formula
   desc "Automated code remediation."
   homepage "https://moderne.io"
-  url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/v1.7.1/moderne-cli-macos-tar-v1.7.1"
-  sha256 "71465b344858440c9d9c9df8a6aed7979791930be12f00be0c08fc4c78725690"
+  url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.27/moderne-cli-macos-tar-v1.6.27"
+  sha256 "d45a251e28030f348f19c413379a7755a85d49a9e465725f78d1185625f7b1d9"
   license :public_domain
-  version "v1.7.1"
+  version "v1.6.27"
+
+  head "https://github.com/moderneinc/homebrew-moderne.git", branch: "staging" # the default is "main"
 
   def install
     bin.install "mod"
