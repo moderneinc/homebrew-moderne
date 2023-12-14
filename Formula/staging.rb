9d4378cd0d264b38625185615951e55ad8c358f1
Joan Viladrosa <joan@moderne.io>
2023-12-14 01:11:36 +0100

    Update staging.rb

--- Formula/staging.rb
+++ Formula/staging.rb
@@ -1,5 +1,5 @@
 module Staging
-  URL = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/CLI_VERSION/moderne-cli-macos-tar-CLI_VERSION"
-  SHA256 = "CLI_SHA256"
-  VERSION "CLI_VERSION"
+  URL = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/v1.7.1/moderne-cli-macos-tar-v1.7.1"
+  SHA256 = "71465b344858440c9d9c9df8a6aed7979791930be12f00be0c08fc4c78725690"
+  VERSION "v1.7.1"
 end
