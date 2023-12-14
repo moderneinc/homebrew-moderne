8e23740411079dd153fd488702df233c6dd42e21
Joan Viladrosa <joan@moderne.io>
2023-12-14 01:11:03 +0100

    Create stable.rb

---
+++ Formula/stable.rb
@@ -0,0 +1,5 @@
+module Stable
+  URL = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.27/moderne-cli-macos-tar-v1.6.27"
+  SHA256 = "d45a251e28030f348f19c413379a7755a85d49a9e465725f78d1185625f7b1d9"
+  VERSION "v1.6.27"
+end
