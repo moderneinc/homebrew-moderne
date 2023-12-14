945ef9d446f84122e9ca94de04f605ab2c1e5e1f
Joan Viladrosa <joan@moderne.io>
2023-12-14 01:11:58 +0100

    Update mod.rb

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -1,22 +1,23 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
+require "stable"
+require "staging"
+
 class Mod < Formula
   desc "Automated code remediation."
   homepage "https://moderne.io"
   license :public_domain
 
   stable do
-    url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/moderne/maven/v1/io/moderne/moderne-cli-macos-tar/v1.6.27/moderne-cli-macos-tar-v1.6.27"
-    sha256 "d45a251e28030f348f19c413379a7755a85d49a9e465725f78d1185625f7b1d9"
-    version "v1.6.27"
+    url Stable::URL
+    sha256 Stable::SHA256
+    version Stable::VERSION
   end
-  
+
   head do
-    url "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/v1.7.1/moderne-cli-macos-tar-v1.7.1"
-    sha256 "71465b344858440c9d9c9df8a6aed7979791930be12f00be0c08fc4c78725690"
-    version "v1.7.1"
+    url Staging::URL
+    sha256 Staging::SHA256
+    version Staging::VERSION
   end
-  
+
   def install
     bin.install "mod"
   end
