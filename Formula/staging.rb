4d0cc7ae1aa089a35ded6ce25c01d769b4506de9
Joan Viladrosa <joan@moderne.io>
2023-12-14 01:10:09 +0100

    Create staging.rb

---
+++ Formula/staging.rb
@@ -0,0 +1,5 @@
+module Staging
+  URL = "https://pkgs.dev.azure.com/moderneinc/moderne_public/_packaging/staging/maven/v1/io/moderne/moderne-cli-macos-tar/CLI_VERSION/moderne-cli-macos-tar-CLI_VERSION"
+  SHA256 = "CLI_SHA256"
+  VERSION "CLI_VERSION"
+end
