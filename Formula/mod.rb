c60c9c3a9c55b6c9f9c18f43e9cbd7a94f5ef7de
Greg Oledzki <greg.oledzki@moderne.io>
2026-02-17 17:24:03 +0100

    Revert to previous formula, keep 3.57.6

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -7,31 +7,21 @@ class Mod < Formula
   license :public_domain
 
   stable do
-    url Stable::URL, using: :nounzip
+    url Stable::URL
     sha256 Stable::SHA256
     version Stable::VERSION
   end
 
   head do
-    url Staging::URL, using: :nounzip
+    url Staging::URL
     sha256 Staging::SHA256
     version Staging::VERSION
   end
 
   def install
-    # The .sh file is a self-extracting archive (shell header + tgz after __ARCHIVE__ marker)
-    system "sh", "-c",
-           "tail -n+$(awk '/^__ARCHIVE__$/{print NR+1;exit}' \"$1\") \"$1\" | tar xzf -",
-           "--", cached_download
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"modw"
-    bin.install_symlink bin/"modw" => "mod"
-
-    # Trigger AOT cache creation
-    system "#{bin}/mod", "--version"
+    bin.install "mod"
   end
-
   test do
-    system "#{bin}/mod", "--version"
-  end
+      system "#{bin}/mod"
+   end
 end
