dd7e182b2153c41e293ddd79c4157699caaa8d37
Nate Danner <nate.danner@gmail.com>
2026-02-27 09:47:24 -0800

    fix brew install

--- Formula/stable.rb
+++ Formula/stable.rb
@@ -1,5 +1,5 @@
 module Stable
-  URL = "https://github.com/moderneinc/moderne-cli-releases/releases/download/v3.57.12/moderne-cli-macos.sh"
+  URL = "https://github.com/moderneinc/moderne-cli-releases/releases/download/v3.57.12/moderne-cli-macos.tar.gz"
   SHA256 = "a9e461b18494cd5a2083fee8b1562156254034d86f444b231f2c416c17f0e0ee"
   VERSION = "v3.57.12"
 end
