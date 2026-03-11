dbb0a010bde57759d7dd6fc81308f968180de3f6
Greg Oledzki <greg.oledzki@moderne.io>
2026-03-11 10:50:20 +0100

    Revert "Update mod to 4.0.4"
    This reverts commit 9786f9f4490c6c2e9598bc7405ecee1f56a443c0.

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -1,17 +1,27 @@
+require_relative "stable"
+require_relative "staging"
+
 class Mod < Formula
   desc "Automated code remediation."
   homepage "https://moderne.io"
   license :public_domain
-  url "https://repo1.maven.org/maven2/io/moderne/moderne-cli/4.0.4/moderne-cli-4.0.4-modw.sh"
-  sha256 "d40133a075d66dae16b5d0e0af46f3ac2594b1ed063406322185a39d449633dd"
-  version "4.0.4"
+
+  stable do
+    url Stable::URL
+    sha256 Stable::SHA256
+    version Stable::VERSION
+  end
+
+  head do
+    url Staging::URL
+    sha256 Staging::SHA256
+    version Staging::VERSION
+  end
 
   def install
-    bin.install "moderne-cli-#{version}-modw.sh" => "modw"
-    bin.install_symlink bin/"modw" => "mod"
+    bin.install "mod"
   end
-
   test do
-    system "#{bin}/mod", "--version"
-  end
+      system "#{bin}/mod"
+   end
 end
