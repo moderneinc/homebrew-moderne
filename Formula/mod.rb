d6b4a73c03e9ae6ecc7f0d306c1d304362c26454
Greg Oledzki <greg.oledzki@moderne.io>
2026-03-11 11:01:31 +0100

    4.0.4 as staging

--- Formula/mod.rb
+++ Formula/mod.rb
@@ -19,7 +19,12 @@ class Mod < Formula
   end
 
   def install
-    bin.install "mod"
+    if head?
+      bin.install "moderne-cli-#{version}-modw.sh" => "modw"
+      bin.install_symlink bin/"modw" => "mod"
+    else
+      bin.install "mod"
+    end
   end
   test do
       system "#{bin}/mod", "--version"
