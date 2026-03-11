78baf39ace22d26da0574ccce6e3238a2563e292
Greg Oledzki <greg.oledzki@moderne.io>
2026-03-11 10:54:21 +0100

    Promote 3.57.14 to stable

--- Formula/stable.rb
+++ Formula/stable.rb
@@ -1,5 +1,5 @@
 module Stable
-  URL = "https://github.com/moderneinc/moderne-cli-releases/releases/download/v3.57.12/moderne-cli-macos.tar.gz"
-  SHA256 = "a9e461b18494cd5a2083fee8b1562156254034d86f444b231f2c416c17f0e0ee"
-  VERSION = "v3.57.12"
+  URL = "https://github.com/moderneinc/moderne-cli-releases/releases/download/v3.57.14/moderne-cli-macos.tar.gz"
+  SHA256 = "6afac583b5203e4c601fefc7e74d13e9f75660fa446eba9f9c96cd096e128ba2"
+  VERSION = "v3.57.14"
 end
