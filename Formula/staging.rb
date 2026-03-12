a15a4f08dd812af45cdbc512199b028fc03d1875
Greg Oledzki <greg.oledzki@moderne.io>
2026-03-12 16:26:46 +0100

    4.05, honoring 3.57.14 (#7)

--- Formula/staging.rb
+++ Formula/staging.rb
@@ -1,5 +1,5 @@
 module Staging
-  URL = "https://repo1.maven.org/maven2/io/moderne/moderne-cli/4.0.4/moderne-cli-4.0.4-modw.sh"
-  SHA256 = "d40133a075d66dae16b5d0e0af46f3ac2594b1ed063406322185a39d449633dd"
-  VERSION = "4.0.4"
+  URL = "https://repo1.maven.org/maven2/io/moderne/moderne-cli/4.0.5/moderne-cli-4.0.5-modw.sh"
+  SHA256 = "0ac0cfca4fd792cc6838609de91e5b4f0bd983001cc13ebd9c1e8267bff34938"
+  VERSION = "4.0.5"
 end
